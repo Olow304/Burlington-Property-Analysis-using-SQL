da408c799bfc7466b4c74773d43576bfd915ae1e
OlowDev <saleban.olow@mymail.champlain.edu>
2018-03-01 13:24:13 -0500

    q1 done

--- burlington_analysis.sql
+++ burlington_analysis.sql
@@ -1,7 +1,13 @@
 use project_one;
 
 #----Question 1: How does the building type affect the house value? ----#
-
+SELECT 
+    rs.Grade,
+    rs.YearBlt
+FROM
+	  `full_data` rs
+ORDER BY 
+	  `CurrentBuildingValue` DESC
 
 #----Question 2: How does year built in effect house value? ----#
 
