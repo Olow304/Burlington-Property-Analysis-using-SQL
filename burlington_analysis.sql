35a7c17ea4890e33c5d48901b365698e77ac009c
OlowDev <saleban.olow@mymail.champlain.edu>
2018-03-01 13:28:02 -0500

    q4 done

--- burlington_analysis.sql
+++ burlington_analysis.sql
@@ -1,13 +1,7 @@
 use project_one;
 
 #----Question 1: How does the building type affect the house value? ----#
-SELECT 
-	rs.Grade,
-        rs.YearBlt
-FROM
-	`full_data` rs
-ORDER BY 
-	 `CurrentBuildingValue` DESC
+
 
 #----Question 2: How does year built in effect house value? ----#
 
@@ -16,7 +10,13 @@ ORDER BY
 
 
 #----Question 4: How does the condition (grade) will affect the house value? ----#
-
+SELECT 
+	rs.Grade, rs.YearBlt
+FROM
+	`full_data` rs
+ORDER BY 
+	 `CurrentBuildingValue` DESC
+# Notes -- Of course the condition of the house affects the house value. It also depends on the year the house was build on. 
 
 #----Question 5: What heating type was most popular in each decade? ----#
 
