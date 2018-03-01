fa66d217a1b2a4cc48f725304fe7ace4be19fa36
OlowDev <saleban.olow@mymail.champlain.edu>
2018-03-01 13:06:52 -0500

    analysis

---
+++ burlington_analysis.sql
@@ -0,0 +1,15 @@
+use project_one;
+
+#----Question 1: How does the building type affect the house value? ----#
+
+
+#----Question 2: How does year built in effect house value? ----#
+
+
+#----Question 3: What building type was most popular in each decade? ----#
+
+
+#----Question 4: How does the condition (grade) will affect the house value? ----#
+
+
+#----Question 5: What heating type was most popular in each decade? ----#
