5f01795db5df24918263d77e6e0bf5c36407f7bf
OlowDev <saleban.olow@mymail.champlain.edu>
2018-03-01 13:00:53 -0500

    create subset from burlington dataset

---
+++ create_full_data.sql
@@ -0,0 +1,18 @@
+use project_one;
+SET SQL_SAFE_UPDATES = 0;
+
+DROP TABLE IF EXISTS full_data;
+CREATE TABLE full_data (c_id Integer PRIMARY KEY AUTO_INCREMENT) 
+as select 
+	`BuildingType`, 
+	`Grade`,
+  `LandUse`,
+	`CurrentBuildingValue`,
+  `PropertyCenterPoint`,
+  `HeatType`,
+  `YearBlt`
+from burlington;
+
+delete from full_data
+where `YearBlt`  = 0;
+select * from full_data;
