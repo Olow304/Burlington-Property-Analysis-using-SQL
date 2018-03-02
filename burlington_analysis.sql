01cca84f115225a9ead91bda569475d6c1134a00
OlowDev <saleban.olow@mymail.champlain.edu>
2018-03-02 18:04:35 -0500

    Update burlington_analysis.sql

--- burlington_analysis.sql
+++ burlington_analysis.sql
@@ -1,23 +1,46 @@
 use project_one;
+SET SQL_SAFE_UPDATES = 0;
+----########################################################################
 
-#----Question 1: How does the building type affect the house value? ----#
-
-
-#----Question 2: How does year built in effect house value? ----#
-
-
-#----Question 3: What building type was most popular in each decade? ----#
-
-
-#----Question 4: How does the condition (grade) will affect the house value? ----#
+# Question 1: How does the building-type affect the house value?
 SELECT 
-	rs.Grade, rs.YearBlt
+	b.BuildingType, c.CurrentValue
 FROM
-	`full_data` rs
-ORDER BY 
-	 `CurrentBuildingValue` DESC
-# see report file for more explanation -- the condition of the house affects the house value. It also depends on the year the house was build on. 
+	build_type b
+JOIN 
+current_value c ON b.AccountNumber = c.AccountNumber;
+----########################################################################
 
-#----Question 5: What heating type was most popular in each decade? ----#
+#----Question 2: How does year-built in affect house value? ----#
+DELETE FROM 
+	year_build
+WHERE 
+	`YearBlt`  = 0
+ORDER BY 
+	`YearBlt` ASC;
+SELECT 
+	y.YearBlt AS decade, v.CurrentValue
+FROM 
+	year_build AS y , current_value AS v;
+----########################################################################
+
+#----Question 3: What building-type was most popular in each decade? ----#
+DELETE FROM 
+	type_data
+WHERE 
+	`YearBlt`  = 0
+ORDER BY 
+	`YearBlt` ASC;
+SELECT 
+	(floor(a.YearBlt / 10) * 10) AS decade, a.BuildingType AS cnt
+FROM 
+	type_data AS a
+GROUP BY 
+	floor(a.YearBlt / 10);
+----########################################################################
+
+#----Question 4: How does the condition(grade) affects the house value? this needs join clause
+
+#----Question 5: What heating-type was most popular in each decade? ----#
 
 
